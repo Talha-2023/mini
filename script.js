8413db1dce58b69c5ac304fb0c95d4f632ef93f7
talha_2024 <syedtalhaahmed123786@gmail.com>
2024-10-09 10:09:22 +0530

    created working fav section

--- script.js
+++ script.js
@@ -40,9 +40,9 @@ const displaySearch = document.querySelector(".displaySearch");
 function favDisplayFun() {
   favorite.style.background = "#d8e8ff";
   search.style.background = "white";
-  displayFavourate.style.display = "block";
+  displayFavourate.style.display = "grid";
   displaySearch.style.display = "none";
-  }
+}
 
 function searchDisplayFun() {
   search.style.background = "#d8e8ff";
@@ -51,3 +51,81 @@ function searchDisplayFun() {
   displaySearch.style.display = "block";
   console.log("search");
 }
+
+//------------heart
+
+document.addEventListener("DOMContentLoaded", function () {
+  const favoriteButtons = document.querySelectorAll(".heart");
+
+  const favoriteCardsContainer = document.querySelector(".displayFavourate");
+
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+  favoriteButtons.forEach((button) => {
+    const card = button.closest(".appName_Container");
+    const cardId = card.getAttribute("data-id");
+
+    if (favorites.includes(cardId)) {
+      button.children[0].setAttribute("src", "Style/Assets/Icons/hearfill.svg");
+      addToFavorites(card);
+    }
+    button.addEventListener("click", function () {
+      if (favorites.includes(cardId)) {
+        favorites = favorites.filter((id) => id !== cardId);
+        button.children[0].setAttribute(
+          "src",
+          "Style/Assets/Icons/hearstroke.svg"
+        );
+        removeFromFavorites(cardId);
+      } else {
+        favorites.push(cardId);
+        button.children[0].setAttribute(
+          "src",
+          "Style/Assets/Icons/hearfill.svg"
+        );
+        addToFavorites(card);
+      }
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+    });
+  });
+
+  function addToFavorites(card) {
+    const cardClone = card.cloneNode(true);
+    cardClone
+      .querySelector(".heart")
+      .setAttribute("src", "Style/Assets/Icons/hearfill.svg");
+    favoriteCardsContainer.appendChild(cardClone);
+
+    cardClone.querySelector(".heart").addEventListener("click", () => {
+      const cardId = cardClone.getAttribute("data-id");
+      favorites = favorites.filter((id) => id !== cardId);
+
+      localStorage.setItem("favorites", JSON.stringify(favorites)); // Update storage
+      cardClone.remove();
+
+      document
+        .querySelector(`.appName_Container[data-id="${cardId}"]`)
+        .querySelector(".heart img")
+        .setAttribute("src", "Style/Assets/Icons/hearstroke.svg");
+    });
+  }
+
+  function removeFromFavorites(cardId) {
+    const cardToRemove = favoriteCardsContainer.querySelector(
+      `.appName_Container[data-id="${cardId}"]`
+    );
+
+    if (cardToRemove) {
+      cardToRemove.remove();
+    }
+  }
+
+  // const messageElement = document.querySelector(".displayWhenCldNotThere");
+  // if (favoriteCardsContainer.children.length === 0) {
+  //   messageElement.style.display = "flex";
+  //   console.log("yes");
+  // } else {
+  //   messageElement.style.display = "none";
+  //   console.log("yes");
+  // }
+});
